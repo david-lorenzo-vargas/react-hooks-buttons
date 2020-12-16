9765d5826ebd931348abaa55da35439b48784d1f
unknown <dlorenzo.05@gmail.com>
2020-12-16 23:31:58 +0100

    Icon component created

---
+++ client/app/components/icon/icon.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames/bind';
+import * as glyphs from './svg';
+
+import styles from './icon.scss';
+
+const cx = classnames.bind(styles);
+
+const Icon = (props) => {
+  const { icon, size } = props;
+
+  return (
+    <div className={cx('icon', {
+      [`icon--size${size}`]: size,
+    })}
+    >
+      <i dangerouslySetInnerHTML={{ __html: glyphs[icon] }} />
+    </div>
+  );
+};
+
+Icon.propTypes = {
+  icon: PropTypes.string,
+  size: PropTypes.string,
+};
+
+Icon.defaultProps = {
+  icon: '',
+  size: '',
+};
+
+export default Icon;

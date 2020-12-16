a5b235a1eb3d2c5d26d0d21143b26221218166d0
unknown <dlorenzo.05@gmail.com>
2020-12-16 20:54:13 +0100

    index.jsx file created

---
+++ client/app/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app';
+
+const rootElement = document.querySelector('#app');
+
+ReactDOM.render(<App />, rootElement);

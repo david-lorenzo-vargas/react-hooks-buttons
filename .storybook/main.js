401b04cd4ecb117c200230b099995daf5b7b6d25
unknown <dlorenzo.05@gmail.com>
2020-12-17 00:32:39 +0100

    storybbok config updated

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,23 @@
+const path = require('path');
+const webpackConfig = require('../webpack.config.js');
+
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../client/app/components/**/*.stories.mdx",
+    "../client/app/components/**/*.stories.@(js|jsx|ts|tsx)",
+    "../client/app/components/**/*.story.@(js|jsx|ts|tsx)",
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: (config) => {
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: webpackConfig({ NODE_ENV: 'development' }).module.rules
+      },
+    };
+  },
 }

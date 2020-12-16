bae7456bdd8db27f25193cfbdaab3300943138ef
unknown <dlorenzo.05@gmail.com>
2020-12-16 23:32:43 +0100

    index for sport default all SVG icons

---
+++ client/app/components/icon/svg/index.js
@@ -0,0 +1,4 @@
+export { default as add } from './add.svg';
+export { default as card } from './card.svg';
+export { default as chart } from './chart.svg';
+export { default as profile } from './profile.svg';

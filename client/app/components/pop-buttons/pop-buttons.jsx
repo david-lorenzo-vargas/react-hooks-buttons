0f5096cb0d050a83659d8a001cf3aefeaf759ad9
unknown <dlorenzo.05@gmail.com>
2020-12-17 16:48:23 +0100

    unecesary props removed, structure updated

--- client/app/components/pop-buttons/pop-buttons.jsx
+++ client/app/components/pop-buttons/pop-buttons.jsx
@@ -6,25 +6,18 @@ import styles from './pop-buttons.scss';
 
 const cx = classnames.bind(styles);
 
-const PopButtons = (props) => {
-  const { active } = props;
-
-  return (
-    <div className={cx('pop-buttons', {
-      'pop-buttons--active': active,
-    })}
-    >
-      <Column>
-        <Row center>
-          <Button size="small" icon="card" />
-        </Row>
-        <Row between>
-          <Button size="small" icon="profile" />
-          <Button size="small" icon="chart" />
-        </Row>
-      </Column>
-    </div>
-  );
-};
+const PopButtons = () => (
+  <div className={cx('pop-buttons')}>
+    <Column>
+      <Row center>
+        <Button size="small" icon="card" />
+      </Row>
+      <Row between>
+        <Button size="small" icon="profile" />
+        <Button size="small" icon="chart" />
+      </Row>
+    </Column>
+  </div>
+);
 
 export default PopButtons;

0039102132cbaa48a8eac0bec079f65b904ff2a4
unknown <dlorenzo.05@gmail.com>
2020-12-17 02:48:53 +0100

    Page component created, state managed with Hooks

---
+++ client/app/components/page/page.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import styles from './page.scss';
+import Button from '../button';
+
+const Page = () => {
+  const [state, setState] = useState({ active: false });
+
+  const handleButtonClick = () => {
+    setState({
+      active: !state.active,
+    });
+  };
+
+  return (
+    <div className={styles.page}>
+      <Button onClick={handleButtonClick} icon="add" size="medium" active={state.active} />
+    </div>
+  );
+};
+
+export default Page;
